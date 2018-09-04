dce20bd3801a5bd9e0e867bcebdf84dfc7355bb0
Tushar <tdebnath@expedia.com>
2018-09-05 02:22:39 +0530

    init

---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+/* eslint-disable */
+
+import React, { Component } from 'react';
+
+export default class App extends Component {
+  render() {
+    return <div>{this.props.name}</div>;
+  }
+}
---
+++ src/index.js
@@ -0,0 +1,9 @@
+/* eslint-disable */
+import React from 'react';
+import ReactDOM from 'react-dom';
+import 'normalize.css';
+
+import App from './App';
+
+const mountNode = document.getElementById('app');
+ReactDOM.render(<App name="Hello World" />, mountNode);
